57f6acd607948ec97755fca84b2a5a8a12012414
KazumaIshide <69346468+KazumaIshide@users.noreply.github.com>
2020-08-30 16:52:04 +0900

    2-18
    関数の部分の修正

--- PHP/2-18/answer.php
+++ PHP/2-18/answer.php
@@ -10,10 +10,24 @@
 <?php 
 //[question.php]から送られてきた名前の変数、選択した回答、問題の答えの変数を作成
 $my_name = $_POST['my_name'];
+
 $portnum = $_POST['portnum'];
 $lang = $_POST['lang'];
 $command = $_POST['command'];
+
+$ans_portnum = 80;
+$ans_lang = "HTML";
+$ans_command = "select";
+
 //選択した回答と正解が一致していれば「正解！」、一致していなければ「残念・・・」と出力される処理を組んだ関数を作成する
+function judge($respons, $answer){
+    if($respons == $answer){
+        echo '正解！';
+    }
+    else{
+        echo '残念・・・';
+    }
+}
 ?>
 
 <br>
@@ -21,29 +35,17 @@ $command = $_POST['command'];
 <p>①の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-    if ($portnum == 80){
-        echo '正解!';
-    }else{
-        echo '残念・・・';
-    }
+judge($portnum, $ans_portnum);
 ?>
 <p>②の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-    if ($lang == "HTML"){
-            echo '正解!';
-    }else{
-            echo '残念・・・';
-    }
+judge($lang, $ans_lang);
 ?>
 <p>③の答え</p>
 <!--作成した関数を呼び出して結果を表示-->
 <?php
-    if ($command == "select"){
-        echo '正解!';
-    }else{
-        echo '残念・・・';
-    }
+judge($command, $ans_command);
 ?>
 </body>
 </html>
